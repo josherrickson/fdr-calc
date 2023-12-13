ffa309557a77964dd6652d5c08ead1838df3dd08
Josh Errickson <josh@errickson.net>
2023-12-13 10:55:31 -0500

    Basic calculations work

---
+++ script.js
@@ -0,0 +1,88 @@
+function updateTable() {
+    const resultTable = document.getElementById("resultTable");
+    const alpha = document.getElementById("alpha");
+
+    //
+
+    // Get p-values and clean them
+    const pvalues = document.getElementById("pvalues");
+    // Split string and convert to numeric
+    let pvArray = pvalues.value.split(",").map(Number);
+    // Drop any non-numeric
+    pvArray = pvArray.filter(element => !isNaN(element));
+    // Drop outside of (0,1]
+    pvArray = pvArray.filter(element => element > 0 & element <= 1);
+
+
+    // Generate Ranks
+    const sortpvals = document.getElementById("sortpvals");
+    let ranks
+    if (sortpvals.checked) {
+        pvArray.sort((a, b) => a - b);
+        ranks = pvArray.map((_, index) => index + 1);
+    } else {
+        ranks = getRankingWithoutSorting(pvArray)
+    }
+
+    // Define critical values
+    let critvals = ranks.map(x => x*alpha.valueAsNumber/pvArray.length);
+    critvals = critvals.map(x => Math.round(x*1000)/1000);
+    console.log(critvals)
+
+
+    // Get significance
+    let largest = 0;
+    for (let i = 0; i < pvArray.length; i++) {
+        console.log(largest)
+        if (pvArray[i] <= critvals[i] && (pvArray[i] > largest)) {
+            largest = pvArray[i];
+        }
+    }
+    let signif = pvArray.map(x => x <= largest)
+
+
+    // Reset table
+    resultTable.innerHTML = '<thead> <tr> <th style="text-align: center;"> p-value </th> <th style="text-align: right;"> Rank </th> <th style="text-align: center;"> BH Critical Value </th> <th style="text-align: left;"> Significant? </th> </tr> </thead> ';
+
+    // Fill up the table
+    for (let i = 0; i < pvArray.length; i++) {
+        // Create a new table row
+        const row = resultTable.insertRow();
+
+        // Create a new table cell (td) for the current number
+        const cell0 = row.insertCell(0);
+        const cell1 = row.insertCell(1);
+        const cell2 = row.insertCell(2);
+        const cell3 = row.insertCell(3);
+
+        // Set the content of the cell to the current number
+        cell0.textContent = pvArray[i];
+        cell1.textContent = ranks[i];
+        cell2.textContent = critvals[i];
+        cell3.textContent = signif[i];
+    }
+}
+
+function getRankingWithoutSorting(array, ascending = true) {
+    const ranks = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let rank = 1; // Start with rank 1
+        for (let j = 0; j < array.length; j++) {
+            if (i !== j) {
+                // Compare the current element with every other element
+                if ((ascending && array[j] < array[i]) || (!ascending && array[j] > array[i])) {
+                    rank++;
+                }
+            }
+        }
+        ranks.push(rank);
+    }
+
+    return ranks;
+}
+
+// Update table on load
+document.addEventListener('DOMContentLoaded', function() {
+    updateTable()
+}, false);
